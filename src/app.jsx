19654d51265698e1b6b36836844164351a8b2ffe
Ruben <ruben@emento.dk>
2025-05-19 16:00:21 +0200

    plural example

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,10 @@
+// Needed in React Native - be sure to check performance on Android
+// https://formatjs.github.io/docs/polyfills/intl-pluralrules#react-native
+import "@formatjs/intl-locale/polyfill";
+import "@formatjs/intl-pluralrules/polyfill-force";
+import "@formatjs/intl-pluralrules/locale-data/en";
+import "@formatjs/intl-pluralrules/locale-data/da";
+import "@formatjs/intl-pluralrules/locale-data/de";
 import { StyleSheet, View } from "react-native";
 import { I18nProvider } from "@lingui/react";
 import { i18n } from "@lingui/core";
--- src/login.jsx
+++ src/login.jsx
@@ -1,6 +1,7 @@
 import { useLingui } from "@lingui/react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { fetchData } from "./fetchData";
+import { useState } from "react";
 
 export const Login = () => {
     const { i18n } = useLingui();
@@ -11,6 +12,45 @@ export const Login = () => {
             <TouchableOpacity onPress={() => fetchData()}>
                 <Text>{i18n.t("Fetch Data")}</Text>
             </TouchableOpacity>
+            <Text>{i18n.number(1234.56)}</Text>
+            <PluralExample />
         </View>
     );
+};
+
+const PluralExample = () => {
+    const { i18n } = useLingui();
+    const [count, setCount] = useState(0);
+    return (
+        <View>
+            <Text>
+                {i18n.t(
+                    "{count, plural, =0 {You have no messages} one {You have one new message} other {You have # new messages}}",
+                    {
+                        count,
+                    }
+                )}
+            </Text>
+            <TouchableOpacity onPress={() => setCount(count + 1)} style={styles.button}>
+                <Text style={styles.buttonText}>Increment</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setCount(count - 1)} style={styles.button}>
+                <Text style={styles.buttonText}>Decrement</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = {
+    button: {
+        backgroundColor: "#007AFF",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        textAlign: "center",
+    },
 };
